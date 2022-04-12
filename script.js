f7a90d5ac8a5216ac619fb87a96c12ae19d8adc6
eimaam <imamddahir@gmail.com>
2022-04-12 12:29:41 +0100

    mobile design start... Nav done!

--- script.js
+++ script.js
@@ -6,5 +6,65 @@ for (i=0; i<accordion.length; i++) {
   })
 }
 
-// LOGIN Authentification:
-let loginB = document.getElementById("login")
+// SIGN UP Authentification:
+// confirm password
+let Spassword = document.getElementById("signUpPass");
+let SconfirmPassword = document.getElementById("signUpPassC");
+function validatePassword(){
+  if(Spassword.value != SconfirmPassword.value){
+    SconfirmPassword.setCustomValidity("Passwords do not match!");
+  }else{
+    SconfirmPassword.setCustomValidity("");
+  }
+}
+Spassword.onchange = validatePassword;
+SconfirmPassword.onkeyup = validatePassword;
+
+let signUpB = document.getElementById("signUp");
+signUpB.addEventListener('click', validatePassword);
+
+// Admin Login:
+let loginPass = document.getElementById("loginPass");
+let loginMail = document.getElementById("loginMail");
+function validateLogin(){
+  // if(loginPass.value === "admin"){
+  //   alert("hhh");
+  // }
+  alert("hhhhh");
+};
+let login = document.getElementById("login");
+login.addEventListener("click", validateLogin);
+
+// Harmburger
+let icon = document.getElementById("hamburger");
+function showHide(){
+let mNav = document.getElementById("mNav");
+  mNav.style.display = "block";
+  let bodyHide = document.getElementById("container");
+  bodyHide.addEventListener('click', hide)
+  function hide(){
+    mNav.style.display = "none";
+    let icon = document.getElementById("hamburger");
+    icon.style.display = "block";
+  let closeIcon = document.getElementById("hideIcon")
+  closeIcon.style.display = "none";
+  }
+}
+// change toggle icon
+function iconHide(){
+  let icon = document.getElementById("hamburger");
+  icon.style.display = "none";
+  let closeIcon = document.getElementById("hideIcon")
+  closeIcon.style.display = "block";
+};
+
+// close mNav
+function hideNav(){
+  let mNav = document.getElementById("mNav");
+  mNav.style.display = "none";
+  let icon = document.getElementById("hamburger");
+  icon.style.display = "block"
+  let closeIcon = document.getElementById("hideIcon")
+  closeIcon.style.display = "none"; 
+}
+
